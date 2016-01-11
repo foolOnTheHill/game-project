c9c8a184d09aa247482c35ca00b707d9c73e5bd0
George Oliveira <ghao@cin.ufpe.br>
2016-01-11 09:32:40 -0300

    simple python server to test the game

---
+++ server.py
@@ -0,0 +1,12 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "Running game at localhost:%d", PORT
+
+httpd.serve_forever()
